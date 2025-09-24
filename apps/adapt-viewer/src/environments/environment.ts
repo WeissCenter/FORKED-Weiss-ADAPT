8e531ddb8694c226d89517fd769be0c6f8ebf718
dmay-aem <dmay@vizuri.com>
2025-09-24 17:48:06 -0400

    env

--- apps/adapt-viewer/src/environments/environment.ts
+++ apps/adapt-viewer/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   envLabel: 'LOCAL',
   contentRoot: 'assets/text',
   contentFileName: 'viewer-content-text.json',
-  appDomain: 'sandbox-viewer.adaptdata.org',
+  appDomain: 'qa-viewer.adaptdata.org',
   defaultContent: 'assets/text/en/viewer-content-text.json',
   API_URL: 'https://y3nityvew3.execute-api.us-east-1.amazonaws.com/qa/',
 };
