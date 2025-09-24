ab9950db4bd2fd54f9e4c62d9dc364f9437ace9f
dmay-aem <dmay@vizuri.com>
2025-09-24 17:17:02 -0400

    Update env

--- apps/adapt-viewer/src/environments/environment.production.ts
+++ apps/adapt-viewer/src/environments/environment.production.ts
@@ -4,5 +4,5 @@ export const environment = {
   contentFileName: 'viewer-content-text.json',
   defaultContent: 'assets/text/en/viewer-content-text.json',
   appDomain: 'qa-viewer.adaptdata.org',
-  API_URL: '',
+  API_URL: 'https://y3nityvew3.execute-api.us-east-1.amazonaws.com/qa/',
 };
