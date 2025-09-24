e841e1e779af90abe33de9715ff401afcb63d2a7
dmay-aem <dmay@vizuri.com>
2025-09-24 17:01:20 -0400

    Temp fix

--- apps/adapt-viewer/src/environments/environment.production.ts
+++ apps/adapt-viewer/src/environments/environment.production.ts
@@ -3,6 +3,6 @@ export const environment = {
   contentRoot: 'assets/text',
   contentFileName: 'viewer-content-text.json',
   defaultContent: 'assets/text/en/viewer-content-text.json',
-  appDomain: 'sandbox-viewer.adaptdata.org',
+  appDomain: 'qa-viewer.adaptdata.org',
   API_URL: '',
 };
