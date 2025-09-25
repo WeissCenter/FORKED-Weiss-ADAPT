504a85c3d60cece0ce6af0d09e5aef80e20d21eb
dmay-aem <dmay@vizuri.com>
2025-09-25 06:33:30 -0400

    Update env vars

--- apps/adapt-viewer/src/environments/environment.production.ts
+++ apps/adapt-viewer/src/environments/environment.production.ts
@@ -3,6 +3,6 @@ export const environment = {
   contentRoot: 'assets/text',
   contentFileName: 'viewer-content-text.json',
   defaultContent: 'assets/text/en/viewer-content-text.json',
-  appDomain: 'qa-viewer.adaptdata.org',
-  API_URL: 'https://y3nityvew3.execute-api.us-east-1.amazonaws.com/qa/',
+  appDomain: 'sandbox-viewer.adaptdata.org',
+  API_URL: 'https://u0oav6271b.execute-api.us-east-1.amazonaws.com/weiss-sandbox/',
 };
